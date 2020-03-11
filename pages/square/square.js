eb923c0a588fed064f1c7ae953ac200f6d3c9d6c
unknown <l2366051620@qq.com>
2020-03-11 11:00:11 +0800

    搜索框

--- pages/square/square.js
+++ pages/square/square.js
@@ -128,4 +128,17 @@ Page({
       url: '/pages/detail/detail?pid=' + e.currentTarget.dataset.pid
     })
   },
+  //搜索输入框输入取值
+  searchInputEvent: function (e) {
+    this.setData({ searchKey: e.detail.value });
+  },
+
+  //搜索按钮点击事件
+  searchClickEvent: function (e) {
+    if (!this.data.searchKey) {
+      return;
+    }
+    this.setData({ pageIndex: 0, pageData: [] });
+    requestData.call(this);
+  },
 })
